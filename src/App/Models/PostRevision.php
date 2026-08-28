ed3448a7f4952cb2fe550fecd3228e1fb0a9cd48
apachish <apachish@gmail.com>
2026-08-29 03:16:44 +0400

    add site map

--- src/App/Models/PostRevision.php
+++ src/App/Models/PostRevision.php
@@ -7,8 +7,10 @@ use Apachish\User\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Sitemap\Contracts\Sitemapable;
+use Spatie\Sitemap\Tags\Url;
 
-class PostRevision extends Model
+class PostRevision extends Model implements Sitemapable
 {
     use HasReadingTime;
 
@@ -17,6 +19,14 @@ class PostRevision extends Model
     protected $table = 'blog_post_revisions';
     protected $fillable = ['post_id', 'user_id', "project_id",'title','slug', 'excerpt', 'content', 'meta_title','meta_description','featured_image','locale','published_at'];
     protected $casts = ['meta' => 'array'];
+
+    public function toSitemapTag(): Url|string|array
+    {
+        return Url::create(route('posts.show', $this->slug))
+            ->setLastModificationDate($this->updated_at)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(0.8);
+    }
 
     public function post(): BelongsTo
     {
